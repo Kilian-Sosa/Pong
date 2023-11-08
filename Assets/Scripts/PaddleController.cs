f056dc8cdde6e72ed9eb705b21a9f557e6b0dafa
Kilian Sosa Guillén <85161810+Kilian-Sosa@users.noreply.github.com>
2023-11-08 17:31:36 +0000

    feat: Control paddle vel.

--- Assets/Scripts/PaddleController.cs
+++ Assets/Scripts/PaddleController.cs
@@ -2,14 +2,16 @@ using UnityEngine;
 
 public class PaddleController : MonoBehaviour {
 
+    [SerializeField] float paddleSpeed; // For controlling the movement vel. of the paddle
+
     void Start() {
         
     }
 
     void Update() {
         float motion = Input.GetAxisRaw("Vertical");
-        if (motion > 0) GetComponent<Rigidbody2D>().velocity = Vector2.up;
-        else if (motion < 0) GetComponent<Rigidbody2D>().velocity = Vector2.down;
+        if (motion > 0) GetComponent<Rigidbody2D>().velocity = Vector2.up * paddleSpeed;
+        else if (motion < 0) GetComponent<Rigidbody2D>().velocity = Vector2.down * paddleSpeed;
         else GetComponent<Rigidbody2D>().velocity = Vector2.zero;
     }
 
