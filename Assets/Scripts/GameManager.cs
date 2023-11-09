125b8260ce841d381777c47c4c619a4ce6e94d9b
Kilian Sosa Guillén <85161810+Kilian-Sosa@users.noreply.github.com>
2023-11-09 18:13:15 +0000

    refactor: Move game functionality to 'GameManager'

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    [SerializeField] float ballSpeed;
+    [SerializeField] GameObject ball, leftPaddle, rightPaddle;
+    [SerializeField] int score1, score2;
+
+    void Start() {
+        SpawnBall();
+    }
+
+    void Update() {
+        if (ball.transform.position.x > rightPaddle.transform.position.x) {
+            score1++;
+            GameObject.Find("Score1").GetComponent<TMP_Text>().text = score1.ToString();
+            SpawnBall();
+        } else if (ball.transform.position.x < leftPaddle.transform.position.x) {
+            score2++;
+            GameObject.Find("Score2").GetComponent<TMP_Text>().text = score2.ToString();
+            SpawnBall();
+        }
+    }
+
+    private void SpawnBall() {
+        ball.transform.position = Vector2.zero;
+        switch (Random.Range(0, 4)) {
+            case 0:
+                ball.GetComponent<Rigidbody2D>().velocity = new Vector2(1, 1) * ballSpeed;
+                break;
+            case 1:
+                ball.GetComponent<Rigidbody2D>().velocity = new Vector2(1, -1) * ballSpeed;
+                break;
+            case 2:
+                ball.GetComponent<Rigidbody2D>().velocity = new Vector2(-1, 1) * ballSpeed;
+                break;
+            case 3:
+                ball.GetComponent<Rigidbody2D>().velocity = new Vector2(-1, -1) * ballSpeed;
+                break;
+        }
+    }
+}
