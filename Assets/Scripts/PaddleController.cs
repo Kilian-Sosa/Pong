22c67179f907c21e5c6ab05226961479c28db406
Kilian Sosa Guillén <85161810+Kilian-Sosa@users.noreply.github.com>
2023-11-10 20:37:29 +0000

    feat: Now the paddle makes a rotation anim

--- Assets/Scripts/PaddleController.cs
+++ Assets/Scripts/PaddleController.cs
@@ -13,5 +13,22 @@ public class PaddleController : MonoBehaviour {
         if (motion > 0) GetComponent<Rigidbody2D>().velocity = Vector2.up * speed;
         else if (motion < 0) GetComponent<Rigidbody2D>().velocity = Vector2.down * speed;
         else GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+            RotatePaddle(1f);
+        else if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+            RotatePaddle(-1f);
+        else if (GetComponent<Rigidbody2D>().rotation > 0 && GetComponent<Rigidbody2D>().rotation % 360 < 10 || 
+                GetComponent<Rigidbody2D>().rotation < 0 && GetComponent<Rigidbody2D>().rotation % 360 > -10) {
+            GetComponent<Transform>().rotation = Quaternion.identity;
+            GetComponent<Rigidbody2D>().freezeRotation = true;
+            GetComponent<Rigidbody2D>().angularVelocity = 0f;
+        }
+    }
+
+    void RotatePaddle(float direction) {
+        GetComponent<Rigidbody2D>().freezeRotation = false;
+        GetComponent<Rigidbody2D>().rotation = direction * 12f;
+        GetComponent<Rigidbody2D>().AddTorque(direction, ForceMode2D.Impulse);
     }
 }
